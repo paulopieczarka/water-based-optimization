4735de6431927661f2b827e1f36010f3876be4d1
Paulo Pieczarka <paulopieczarka@gmail.com>
2019-04-02 18:17:08 -0300

    Impl initial wwo

---
+++ index.js
@@ -0,0 +1,15 @@
+const WWO = require('./lib/wwo')
+const fs = require('fs')
+
+const data = fs.readFileSync('./datasets/soybean-small.data')
+
+const labels = { 'D1': 0, 'D2': 1, 'D3': 2, 'D4': 3 }
+
+const dataset = data.toString().split('\n').map(x => {
+  const values = x.split(',')
+  const feature = values.pop()
+  return { values: values.map(i => +i), feature: labels[feature] }
+})
+
+const wwo = new WWO()
+wwo.selectOptimalFeatures(dataset)
