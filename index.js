a5e8e0e20e1b961df45b3c29b85c5c3dde3d55f1
Paulo Pieczarka <paulopieczarka@gmail.com>
2019-04-03 15:37:06 -0300

    Finish wwo implementation

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const WWO = require('./lib/wwo')
 const fs = require('fs')
 
-const data = fs.readFileSync('./datasets/soybean-small.data')
+const data = fs.readFileSync('./datasets/plat-sizes.data')
 
-const labels = { 'D1': 0, 'D2': 1, 'D3': 2, 'D4': 3 }
+// const labels = { 'D1': 0, 'D2': 1, 'D3': 2, 'D4': 3 }
 
 const dataset = data.toString().split('\n').map(x => {
   const values = x.split(',')
   const feature = values.pop()
-  return { values: values.map(i => +i), feature: labels[feature] }
+  return { values: values.map(i => +i), feature: +feature }
 })
 
 const wwo = new WWO()
